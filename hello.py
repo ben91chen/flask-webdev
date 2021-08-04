3a75c93c080fe8e1ef15f7605fe63c84fa2fabed
ben91chen <bc9938@gmail.com>
2021-08-03 21:32:37 -0600

    commit hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 
@@ -37,11 +42,20 @@ class User(db.Model):
     def __repr__(self):
         return f"<User {self.username}>"
 
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
+bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.route('/about')
 def about_us():
